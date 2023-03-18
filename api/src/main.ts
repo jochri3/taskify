2fed262447567bad69f9c84fd14cbd84a2aeab39
Christian Lisangola <jochri3@gmail.com>
2023-03-18 10:28:52 +0300

    Generate swagger json file for Postman import

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,20 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
-    .setTitle('Contact App API')
-    .setDescription('The Contact App API description')
+    .setTitle('Taskify API')
+    .setDescription('The Taskify API description')
     .setVersion('0.1')
     .addBearerAuth()
     .build();
 
-
   const document = SwaggerModule.createDocument(app, config);
+  fs.writeFileSync('./swagger-spec.json', JSON.stringify(document));
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
